6991eff1348aa49410c428a5b61bf7194fa54cae
Kara Bayse <karabayse@gmail.com>
2019-01-04 15:54:06 -0600

    removed event-stream and flatmap-stream

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // USES
@@ -13,6 +15,9 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to database
 mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5123;
 
